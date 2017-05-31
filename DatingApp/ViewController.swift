16dcf37ca9d2f27b36a45f8c00ae9f6132a0201f
Apro <aprofromuk-mail@yahoo.co.in>
2017-05-31 17:14:15 +0200

    fixed last person not displayed.

--- DatingApp/ViewController.swift
+++ DatingApp/ViewController.swift
@@ -13,23 +13,23 @@ import MDCSwipeToChoose
 
 private let cardsToShow = 3
 
-final class ViewController: UIViewController, MDCSwipeToChooseDelegate {
-    
+final class ViewController: UIViewController {
+
     private let disposeBag = DisposeBag()
-    private let viewModel = ViewModel(repository: PersonRepository.instance)
-    private var swipeViews = [PersonSwipeView]() // TODO: - implement a queue
+    fileprivate let viewModel = ViewModel(repository: PersonRepository.instance)
+    fileprivate var swipeViews = [PersonSwipeView]() // TODO: - use/implement a queue
     @IBOutlet private var wildCardLabel: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         viewModel.fetchWeatherList()
-        
+
         setupNetworkActivityIndicator()
         setupWildCardLabel()
         setupSwipeView()
     }
-    
+
     private func setupSwipeView() {
         let options = MDCSwipeToChooseViewOptions()
         options.delegate = self
@@ -37,9 +37,9 @@ final class ViewController: UIViewController, MDCSwipeToChooseDelegate {
         options.likedColor = UIColor.green
         options.nopeText = NSLocalizedString("No", comment: "")
         options.nopeColor = UIColor.red
-        
+
         let rect = self.view.bounds
-        
+
         viewModel.people.asDriver().drive(onNext: { [unowned self] people in
             for (index, person) in people.prefix(3).enumerated() {
                 let swipeView = PersonSwipeView(person: person,
@@ -50,38 +50,40 @@ final class ViewController: UIViewController, MDCSwipeToChooseDelegate {
             }
             self.wildCardLabel.text = NSLocalizedString("\(people.count) wildcard matches remaining.", comment: "")
         }).addDisposableTo(disposeBag)
-        
+
         viewModel.currentPersonIndex.asDriver().filter({ [unowned self] idx in
-            self.viewModel.people.value.count > idx + cardsToShow
+            self.viewModel.people.value.count >= idx + cardsToShow
         }).drive(onNext: { [unowned self] idx in
-            
+
             for (idx, view) in self.swipeViews.enumerated() {
                 view.frame.origin = CGPoint(x: 10, y: 100 - idx * 5)
             }
-            
+
             let swipeView = PersonSwipeView(person: self.viewModel.people.value[idx + cardsToShow - 1],
-                frame: CGRect(x: 10, y: 90, width: rect.width - 20, height: rect.height - 200),
-                options: options)
+                                            frame: CGRect(x: 10, y: 90, width: rect.width - 20, height: rect.height - 200),
+                                            options: options)
             self.swipeViews.append(swipeView)
             self.view.insertSubview(swipeView, at: 0)
         }).addDisposableTo(disposeBag)
     }
-    
+
     private func setupNetworkActivityIndicator() {
         viewModel.networkReqOngoing.asDriver()
             .drive(onNext: { ongoing in
                 UIApplication.shared.isNetworkActivityIndicatorVisible = ongoing
             }).addDisposableTo(disposeBag)
     }
-    
+
     private func setupWildCardLabel() {
         viewModel.currentPersonIndex.asDriver().drive(onNext: { [unowned self] idx in
             self.wildCardLabel.text = NSLocalizedString("\(self.viewModel.people.value.count - idx) wildcard matches remaining.",
-                comment: "")
+                                                        comment: "")
         }).addDisposableTo(disposeBag)
     }
-    
-    // MARK: - MDCSwipeViewDelegate
+}
+
+extension ViewController: MDCSwipeToChooseDelegate {
+
     func view(_ view: UIView!, wasChosenWith direction: MDCSwipeDirection) {
         swipeViews.remove(at: 0)
         view.removeFromSuperview()
